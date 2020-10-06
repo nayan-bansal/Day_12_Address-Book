2c4b06c43f74a2cfb3c2363f9071e1365c83d8b1
nayan-bansal <nayan.7991@gmail.com>
2020-10-06 23:27:42 +0530

    Added: List of all Contacts Added

---
+++ AddressBookLog.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class AddressBookLog {
+
+	
+	public void addContact(ContactPerson person) {
+		
+		ArrayList<ContactPerson> directory = new ArrayList<ContactPerson>();
+		
+		directory.add(person);
+		
+	}
+	
+}
