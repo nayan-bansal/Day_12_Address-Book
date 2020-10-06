e3e928d16ec6d3b547e95cdf17e0907a88d9ce27
nayan-bansal <nayan.7991@gmail.com>
2020-10-07 04:31:15 +0530

    Added: Contacts in Dictionary

---
+++ AddressBookDic.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class AddressBookDic {
+	private HashMap<String, ContactPerson> dictionary =new HashMap<>();
+	
+	
+	public void setDictionary(HashMap<String, ContactPerson> dictionary) {
+		this.dictionary = dictionary;
+	}
+	
+	public HashMap<String, ContactPerson> getDictionary(){
+		return dictionary;
+	}
+
+	
+	public void addDic(String name, ContactPerson person) {
+		dictionary.put(name, person);
+	}
+
+	public int lenDictionary() {
+		return dictionary.size();
+	}
+
+}
