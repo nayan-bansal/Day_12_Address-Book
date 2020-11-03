e13db376e5f0613749603022ae7c57073dea2ebe
nayan-bansal <nayan.7991@gmail.com>
2020-11-03 16:41:04 +0530

    Added: retrieving data for particular date range

--- AddressBookService.java
+++ AddressBookService.java
@@ -1,5 +1,6 @@
 package day.address.book;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookService {
@@ -20,5 +21,27 @@ public class AddressBookService {
 		this.contactList = addressBookDBService.readData();
 		return contactList;
 	}
-	
+
+	public void updateContactDetails(String name, String address) {
+		int result = addressBookDBService.updateEmployeeData(name, address);
+		if (result == 0)
+			return;
+		ContactPerson personInfo = this.getContactData(name);
+		if (personInfo != null)
+			personInfo.address = address;
+	}
+
+	private ContactPerson getContactData(String name) {
+		return this.contactList.stream().filter(contact -> contact.firstName.equals(name)).findFirst().orElse(null);
+	}
+
+	public boolean checkConatctDetailsInSyncWithDB(String name) {
+		List<ContactPerson> contactList = addressBookDBService.getcontactData(name);
+		return contactList.get(0).equals(getContactData(name));
+	}
+
+	public List<ContactPerson> readContactDataForDateRange(LocalDate startDate, LocalDate endDate) {
+		this.contactList = addressBookDBService.getContactForDateRange(startDate, endDate);
+		return contactList;
+	}
 }
