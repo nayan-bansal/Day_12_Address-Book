5db3ee0ec1a64b1ca6e9d2fb4621507c17bcfc69
nayan-bansal <nayan.7991@gmail.com>
2020-11-03 16:12:03 +0530

    Added: Retrieve all entries from the DB

---
+++ AddressBookDBService.java
@@ -0,0 +1,78 @@
+package day.address.book;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookDBService {
+
+	private static AddressBookDBService addressBookDBService;
+
+	private AddressBookDBService() {
+	}
+
+	static AddressBookDBService getInstance() {
+		if (addressBookDBService == null) {
+			addressBookDBService = new AddressBookDBService();
+		}
+		return addressBookDBService;
+	}
+
+	public Connection getConnection() throws SQLException {
+		String jdbcURL = "jdbc:mysql://localhost:3307/address_book_service?useSSL=false";
+		String userName = "root";
+		String password = "nayan@1965";
+		Connection connection;
+		System.out.println("connecting to database: " + jdbcURL);
+		connection = DriverManager.getConnection(jdbcURL, userName, password);
+		System.out.println("connection successful !!!! " + connection);
+		return connection;
+	}
+
+	public List<ContactPerson> readData() {
+		String sql = "SELECT c.first_name, c.last_name,c.address_book_name,c.address,c.city,"
+				+ "c.state,c.zip,c.phone_number,c.email,abd.address_book_type "
+				+ "from contact_details c inner join address_book_dict abd "
+				+ "on c.address_book_name=abd.address_book_name; ";
+		return this.getContactDetailsUsingSqlQuery(sql);
+	}
+
+	private List<ContactPerson> getContactDetailsUsingSqlQuery(String sql) {
+		List<ContactPerson> ContactList = null;
+		try (Connection connection = addressBookDBService.getConnection();) {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			ResultSet result = preparedStatement.executeQuery(sql);
+			ContactList = this.getAddressBookData(result);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ContactList;
+	}
+
+	private List<ContactPerson> getAddressBookData(ResultSet result) {
+		List<ContactPerson> contactList = new ArrayList<>();
+		try {
+			while (result.next()) {
+				String firstName = result.getString("first_name");
+				String lastName = result.getString("last_name");
+				String addressBookName = result.getString("address_book_name");
+				String address = result.getString("address");
+				String city = result.getString("city");
+				String state = result.getString("state");
+				String zip = result.getString("zip");
+				String phoneNumber = result.getString("phone_number");
+				String email = result.getString("email");
+				String addressBookType = result.getString("address_book_type");
+				contactList.add(new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email,
+						addressBookName, addressBookType));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+}
