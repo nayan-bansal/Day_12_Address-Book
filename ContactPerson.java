0f02964211c903268e3ad516c5513ba0a61d395f
nayan-bansal <nayan.7991@gmail.com>
2020-11-03 16:11:34 +0530

    Added: Retrieve all entries from the DB

--- ContactPerson.java
+++ ContactPerson.java
@@ -1,93 +1,45 @@
 package day.address.book;
 
 public class ContactPerson {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public String zip;
+	public String phoneNo;
+	public String email;
+	public String addressBookName;
+	public String addressBookType;
 
-	private String first_name;
-	private String last_name;
-	private String address;
-	private String city;
-	private String state;
-	private int zip_code; 
-	private double phone_number;
-	private String email_Id;
-	
-	
-	public ContactPerson() {
+	public ContactPerson(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNo, String email) {
 		super();
-		this.first_name = first_name;
-		this.last_name = last_name;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.address = address;
 		this.city = city;
 		this.state = state;
-		this.zip_code = zip_code;
-		this.phone_number = phone_number;
-		this.email_Id = email_Id;
-	}
-	
-	
-	public String getFirst_name () {
-		return first_name;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.email = email;
 	}
 
-	
-	public void setFirst_name(String first_name) {
-		this.first_name  = first_name;
+	public ContactPerson(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNo, String email, String addressBookName, String addressBookType) {
+		this(firstName, lastName, address, city, state, zip, phoneNo, email);
+		this.addressBookName = addressBookName;
+		this.addressBookType = addressBookType;
 	}
-	
-	public String getLast_name () {
-		return last_name;
+
+	public ContactPerson() {
 	}
-	
-	public void setLast_name(String last_name) {
-		this.last_name  = last_name;
-	}
-	public String getAddress () {
-		return address;
-	}
-	
-	public void setAddress(String address) {
-		this.address  = address;
-	}
-	public String getState () {
-		return state;
-	}
-	
-	public void setState (String state) {
-		this.state  = state;
-	}
-	
-	public String getCity () {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city  = city;
-	}
-	public int getZip_code () {
-		return zip_code;
-	}
-	
-	public void setZip_code(int zip_code) {
-		this.zip_code  = zip_code;
-	}
-	public String getEmail_Id () {
-		return email_Id;
-	}
-	
-	public void setPhone_number(double phone_number) {
-		this.phone_number  = phone_number;
-	}
-	public double getPhone_number () {
-		return phone_number;
-	}
-	
-	public void setEmail_Id(String email_Id) {
-		this.email_Id  = email_Id;
-	}
-	
+
+	@Override
 	public String toString() {
-		return "First Name: " + this.first_name + " Last Name: " + this.last_name + " Address: " + this.address
-				+ " City: " + this.city + " State: " + this.state + " Zip: " + this.zip_code + " Phone Number: "
-				+ this.phone_number + " Email: " + this.email_Id;
+		return "First Name: " + this.firstName + " Last Name: " + this.lastName + " Address: " + this.address
+				+ " City: " + this.city + " State: " + this.state + " Zip: " + this.zip + " Phone Number: "
+				+ this.phoneNo + " Email: " + this.email + " Address book name" + this.addressBookName + " type"
+				+ addressBookType;
 	}
-	
 }
